575ac8c9cb3e83d793bf5bb5b142fa0d189136d3
João Alexande <72232422+Jadc-1@users.noreply.github.com>
2024-09-07 01:05:03 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -5,8 +5,8 @@ let filmes_netflix = [
         ano: 2018,
         sinopse: "O filme Bird Box é um thriller de suspense e ficção científica lançado em 2018, dirigido por Susanne Bier e estrelado por Sandra Bullock. A história se passa em um mundo pós-apocal íptico onde criaturas misteriosas fazem com que as pessoas que as vejam se tornem violentas e cometam suicídio. A personagem Malorie Hayes, interpretada por Sandra Bullock, precisa proteger seus filhos e a si mesma dessas criaturas. Para isso, ela precisa navegar por um rio com os olhos vendados para evitar ver as criaturas.",
         duracao: "124 min",
-        atores: ["Sandra Bullock", "Trevante Rhodes", "John Malkovich"],
-        autor: "Susanne Bier"
+        atores: ["Sandra Bullock", "Trevante Rhodes", "John Malkovich", "mais"],
+        autor: "Susanne Bier",
       },
       {
         cartaz: "imagens/imagens_poster/resgate_poster.jpg",
@@ -14,7 +14,7 @@ let filmes_netflix = [
         ano: 2020,
         sinopse: "O filme Resgate (também conhecido como Ransom) é um thriller de ação lançado em 1996, dirigido por Ron Howard e estrelado por Mel Gibson. A história segue Tom Mullen, um empresário rico cujo filho é sequestrado por um grupo de criminosos que exigem um resgate. No entanto, Tom decide não pagar o resgate e, em vez disso, oferece uma recompensa para quem puder fornecer informações sobre o paradeiro de seu filho, o que leva a uma série de eventos intensos e perigosos. Ao longo do filme, Tom precisa lidar com a pressão da mídia, a polícia e os sequestradores, enquanto luta para resgatar seu filho e trazer os criminosos à justiça.",
         duracao: "116 min",
-        atores: ["Chris Hemsworth", "Rudhraksh Jaiswal", "Randeep Hooda"],
+        atores: ["Chris Hemsworth", "Rudhraksh Jaiswal", "Randeep Hooda", "mais"],
         autor: "Sam Hargrave"
       },
       {
@@ -23,24 +23,61 @@ let filmes_netflix = [
         ano: 2019,
         sinopse: "O filme Esquadrão 6 é um thriller de ação lançado em 2019, dirigido por Michael Bay e estrelado por Ryan Reynolds. A história segue um bilionário que finge sua própria morte para criar um grupo de elite para combater o crime e mudar o mundo, sem ser identificado. O grupo, formado por pessoas com habilidades especiais, trabalha para derrubar governos tirânicos e instaurar a democracia forçada. No entanto, o herói escolhido para substituir um ditador não pretende seguir a carreira política, mas é gentilmente sequestrado e forçado pelos mocinhos a assumir o governo.",
         duracao: "128 min",
-        atores: ["Ryan Reynolds", "Mélanie Laurent", "Manuel Garcia-Rulfo"],
+        atores: ["Ryan Reynolds", "Mélanie Laurent", "Manuel Garcia-Rulfo", "mais"],
         autor: "Michael Bay"
       },
       {
-        cartaz: "imagens/imagens_poster/enola_holmes.jpg",
+        cartaz: "imagens/imagens_poster/enola.jpg",
         titulo: "Enola Holmes",
         ano: 2020,
         sinopse: "O filme Enola Holmes é um thriller de ação e aventura lançado em 2020, dirigido por Harry Bradbeer e estrelado por Millie Bobby Brown. A história segue Enola Holmes, a irmã mais nova de Sherlock Holmes, que foge de casa para encontrar sua mãe desaparecida em Londres. No entanto, Enola logo descobre que sua mãe está envolvida em uma conspiração política que pode alterar o curso da história. Enola precisa usar suas habilidades de detetive para desvendar o mistério e encontrar sua mãe, ao mesmo tempo em que enfrenta obstáculos e perigos em seu caminho. Além disso, o filme também explora a relação entre Enola e seu irmão Sherlock, que é interpretado por Henry Cavill.",
         duracao: "123 min",
-        atores: ["Millie Bobby Brown", "Henry Cavill", "Sam Claflin"],
+        atores: ["Millie Bobby Brown", "Henry Cavill", "Sam Claflin", "mais"],
         autor: "Harry Bradbeer"
       },
       {
         cartaz: "imagens/imagens_poster/o_irlandes.jpg",
-        titulo: "O irlandês",
+        titulo: "O Irlandês",
         ano: 2019,
         sinopse: "O Irlandês é um filme de drama e crime dirigido por Martin Scorsese, lançado em 2019. A história segue a vida de Frank Sheeran (interpretado por Robert De Niro), um veterano da Segunda Guerra Mundial que se torna um assassino de aluguel para a máfia italiana. Ao longo do filme, Frank relata sua vida e suas experiências, incluindo seu envolvimento no desaparecimento do líder sindical Jimmy Hoffa (interpretado por Al Pacino). Com uma narrativa não linear e uma abordagem reflexiva, o filme explora temas como lealdade, amizade e a busca por redenção, apresentando uma visão crítica e emocional da vida de um homem que esteve no centro de alguns dos eventos mais importantes da história americana.",
-        atores: ["Robert De Niro", "Al Pacino", "Joe Pesci"],
+        atores: ["Robert De Niro", "Al Pacino", "Joe Pesci", "mais"],
         autor: "Martin Scorsese"
+      },
+      {
+        cartaz: "imagens/imagens_poster/poderoso.jpg",
+        titulo: "O Poderoso Chefão",
+        ano: 1972,
+        sinopse: "O Poderoso Chefão é um clássico do cinema que narra a saga da família Corleone, uma poderosa máfia italiana nos Estados Unidos. A trama se concentra em Vito Corleone, o patriarca da família, e sua jornada para construir e manter seu império. O filme explora temas como lealdade, família, poder e corrupção, enquanto acompanhamos a ascensão de Michael Corleone, o filho mais novo de Vito, que se torna um relutante líder da família. Em meio a traições, assassinatos e guerras entre famílias rivais, O Poderoso Chefão oferece uma visão profunda do submundo do crime organizado e do custo pessoal do poder.",
+        duracao: "175 min",
+        atores: ["Marlon Brando", "Al Pacino", "James Caan", "mais"],
+        autor: "Francis Ford Coppola"
+      },
+      {
+        cartaz: "imagens/imagens_poster/panico.png",
+        titulo: "Pânico V",
+        ano: "2022",
+        duracao: "2h 3m",
+        sinopse: "Pânico V nos leva de volta à pacata cidade de Woodsboro, 25 anos após os eventos traumáticos dos primeiros filmes. Um novo assassino, vestindo a icônica máscara de Ghostface, aterroriza a comunidade e revive os traumas do passado. A trama gira em torno de um novo grupo de adolescentes que se tornam alvos do misterioso assassino. Enquanto isso, os sobreviventes originais, Sidney Prescott, Gale Weathers e Dewey Riley, retornam para ajudar a desvendar os mistérios e proteger os jovens de Woodsboro. O filme é uma mistura de homenagem aos filmes anteriores e uma reinvenção da franquia, com novos personagens e reviravoltas inesperadas que mantêm a tensão e o suspense até o final.",
+        atores: ["Jenna Ortega", "Neve Campbell", "Courteney Cox", "mais"],
+        autor: "Matt Bettinelli-Olpin",
+
+      },
+      {
+        cartaz: "imagens/imagens_poster/branquelas.jpg",
+        titulo: "As Branquelas",
+        duracao: "1h 49m",
+        ano: "2004",
+        sinopse: "As Branquelas é uma comédia que acompanha os irmãos Marcus e Kevin Copeland, dois atrapalhados agentes do FBI, que recebem a missão de proteger as mimadas irmãs Wilson, Brittany e Tiffany, durante suas férias. Para se infiltrarem na alta sociedade e garantirem a segurança das garotas, os agentes resolvem se disfarçar e assumir as identidades das irmãs. Com uma série de confusões e situações cômicas, os irmãos se veem em uma aventura hilária, tentando equilibrar a vida de agentes do FBI com a de patricinhas, enquanto tentam desvendar um plano de sequestro.",
+        atores: ["Shawn Wayans", "Marlon Wayans", "Terry Crews", "mais"],
+        autor: "Keenen Ivory Wayans"
+      },
+      {
+        cartaz: "imagens/imagens_poster/minha_mae.jpg",
+        titulo: "Minha mae é uma peça",
+        duracao: "1h 24m",
+        ano: "2013",
+        sinopse: "Minha Mãe É Uma Peça é uma comédia brasileira que gira em torno da personagem Dona Hermínia, interpretada por Paulo Gustavo. Uma mulher de meia idade, divorciada e bastante apegada aos seus filhos adultos, Marcelina e Juliano. Hiperativa e com um senso de humor peculiar, Dona Hermínia enfrenta uma crise existencial ao perceber que seus filhos estão crescendo e não precisam mais dela da mesma forma. A comédia se desenrola com situações hilárias e emocionantes, enquanto Dona Hermínia tenta lidar com a nova fase da vida e a relação com seus filhos, que a consideram um tanto exagerada e invasiva. O filme aborda temas como a família, a maternidade e a busca pela identidade, com muito humor e leveza.",
+        atores: ["Paulo Gustavo", "Mariana Xavier", "Rodrigo Pandolfo", "mais"],
+        autor: "Paulo Gustavo"
       }
 ]
